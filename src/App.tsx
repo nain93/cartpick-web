d2e5dadbaab25678d440b75295839b210cb0140e
nain93 <rnrb555@gmail.com>
2022-03-29 11:40:38 +0900

    초기세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import style from "App.module.scss"
+import Main from "pages/main";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
 	return (
-		<div>
-			matchupickchu-web
+		<div className={style.container}>
+			<Routes>
+				<Route path="/" element={<Main />} />
+			</Routes>
 		</div>
 	);
 }
---
+++ src/styles/theme/color.ts
@@ -0,0 +1,36 @@
+const main = "#7857FF"
+
+const grayscale = {
+	FFFFF: '#FFFFF',
+	F5F5F5: "#F5F5F5",
+	F2F3F6: "#F2F3F6",
+	DFE4EE: "#DFE4EE",
+	B7C3D4: "#B7C3D4",
+	C_4C5463: "#4C5463",
+	black: "#000000",
+};
+
+const PositiveGreen = "#8EC285"
+
+const NegativeRed = "#FFA4A1"
+
+const marketColor = {
+	kurly: "#965FCC",
+	cookat: "#EB5500",
+	oasis: "#6AA337",
+	naver: "#04C73B",
+	emart: "#FFD040",
+	wingIt: "#F95E5E",
+	roket: "#1A982E",
+	other: "#535860"
+}
+
+const color = {
+	main,
+	grayscale,
+	PositiveGreen,
+	NegativeRed,
+	marketColor
+};
+
+export default color;
